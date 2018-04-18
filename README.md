8869dbf442677340bd0815e825502b63dc4e982a
teralove <terascripts@gmail.com>
2018-04-18 01:37:09 -0400

    update 1.1

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Hide Thralls
 Make thralls invisible. (Can be useful for removing camera-shake effects caused by thralls).
+
+## Usage
+### `hidethralls`
+- Toggle off/on
+- Default on
+
+## Changelog
+<details>
+
+### 1.1.0
+* [+] 
+
+    1.10
+    - Added awakening thralls
+    - Added Command toggle
+    
+</details>
--- index.js
+++ index.js
@@ -1,17 +1,31 @@
+const Command = require('command');
+
 module.exports = function HideThralls(dispatch) {
     
-    Thrall_IDs = [
+    const Thrall_IDs = [
         10235016, //Protection
+        10235017, //Protection 2
         10236013, //Life
+        10236014, //Life 2
         10237014, //Vengeance
-        10238007  //Wrath
+        10237015, //Vengeance 2
+        10238007, //Wrath
+        10238008, //Wrath 2
+        10239003  //Lord
     ];
-    HuntingZone_ID = 1023;
+    const HuntingZone_ID = 1023;
+    let enabled = true;
     
     dispatch.hook('S_SPAWN_NPC', 5, (event) => {
+        if (!enabled) return;
         if (Thrall_IDs.includes(event.templateId) && event.huntingZoneId == HuntingZone_ID) {
             return false;
         }
     })
     
+    const command = Command(dispatch);
+    command.add('hidethralls', ()=> {
+        enabled = !enabled;
+        command.message('(hide-thralls) ' + (enabled ? 'enabled' : 'disabled'));
+    });
 }
